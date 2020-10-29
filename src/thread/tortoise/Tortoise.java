10d0659af134d441932e610469f71895e7f2236d
kirtimansinghcs19 <61340145+kirtimansinghcs19@users.noreply.github.com>
2020-10-29 17:52:26 +0530

    Completed

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,40 @@
 package thread.tortoise;
 
-public class Tortoise {
+import javax.swing.*;
+
+public class Tortoise extends Thread {
+    public static final int MILESTONES = 5;
+    private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread( this, "Tortoise" );
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0 -> System.out.println( "The Tortoise has started from the start line!" );
+                case 1 -> System.out.println( "The Tortoise has reached the Cool River!" );
+                case 2 -> System.out.println( "The Tortoise has reached the Mountain Hill!" );
+                case 3 -> System.out.println( "The Tortoise has reached the Big Oak Tree!" );
+                case 4 -> System.out.println( "The Tortoise has reached the finish line!" );
+            }
+            try {
+                Thread.sleep( 3000L );
+            } catch (InterruptedException e) {
+                System.err.println( "The Tortoise has lost the path!" );
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
+    }
 }
